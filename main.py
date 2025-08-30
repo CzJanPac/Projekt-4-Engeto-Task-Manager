098b270304f1acae0ab64ad5eb83d0e8a6d33d69
CzJanPac <czjanpac@gmail.com>
2025-08-30 22:26:54 +0200

    Napojeni s VScode

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ email: czjanpac@gmail.com
 """
 
 ukoly = []
+
+def hlavni_menu():
+    print("""
+Správce úkolů - Hlavní menu
+1. Přidat nový úkol
+2. Zobrazit všechny úkoly
+3. Odstranit úkol
+4. Konec programu""")
+    
+hlavni_menu()
